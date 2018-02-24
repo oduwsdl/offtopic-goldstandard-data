46cd5f9521537a3c49afe353c5b6f69fccac3aed
Shawn M. Jones <jones.shawn.m@gmail.com>
2018-02-24 10:19:30 -0700

    solidified place for datasets and updated comparison script

--- scripts/compare_gold_standard_data_results.py
+++ scripts/compare_gold_standard_data_results.py
@@ -5,9 +5,9 @@ def convert_datafile_to_dict(datafilename):
 
     urim_topic_ratings = {}
 
-    with open(gold_standard_file) as tsvfile:
+    with open(datafilename) as tsvfile:
 
-        reader = csv.DictReader(delimiter='\t')
+        reader = csv.DictReader(tsvfile, delimiter='\t')
 
         for row in reader:
 
@@ -29,34 +29,47 @@ if __name__ == '__main__':
 
     gold_standard_urim_topic_ratings = \
         convert_datafile_to_dict(gold_standard_file)
-    off_topic_output_topic_ratings = \
+    off_topic_output_ratings = \
         convert_datafile_to_dict(off_topic_output_file)
 
     true_positives = []
     false_positives = []
     true_negatives = []
     false_negatives = []
+    missing = []
 
     for urim in gold_standard_urim_topic_ratings:
 
-        if gold_standard_urim_topic_ratings[urim] == True \
-            and off_topic_output_ratings[urim] == True:
-                true_positives.append(urim) 
+        if urim in off_topic_output_ratings:
 
-        elif gold_standard_urim_topic_ratings[urim] == False \
-            and off_topic_output_ratings[urim] == False:
-                true_negatives.append(urim)
+            if gold_standard_urim_topic_ratings[urim] == True \
+                and off_topic_output_ratings[urim] == True:
+                    true_positives.append(urim) 
 
-        elif gold_standard_urim_topic_ratings[urim] == True \
-            and off_topic_output_ratings[urim] == False:
-                false_negatives.append(urim)
+            elif gold_standard_urim_topic_ratings[urim] == False \
+                and off_topic_output_ratings[urim] == False:
+                    true_negatives.append(urim)
 
-        elif gold_standard_urim_topic_ratings[urim] == False \
-            and off_topic_output_ratings[urim] == True:
-                false_positives.append(urim)
+            elif gold_standard_urim_topic_ratings[urim] == True \
+                and off_topic_output_ratings[urim] == False:
+                    false_negatives.append(urim)
+
+            elif gold_standard_urim_topic_ratings[urim] == False \
+                and off_topic_output_ratings[urim] == True:
+                    false_positives.append(urim)
+
+        else:
+            missing.append(urim)
 
 
     print("TP: {}".format(len(true_positives)))
     print("TN: {}".format(len(true_negatives)))
     print("FP: {}".format(len(false_positives)))
     print("FN: {}".format(len(false_negatives)))
+    print("Missing: {}".format(len(missing)))
+    print("Total: {}".format(sum([
+        len(true_positives),
+        len(true_negatives),
+        len(false_positives),
+        len(false_negatives)
+    ])))
